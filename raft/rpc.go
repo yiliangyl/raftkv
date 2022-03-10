6bf31797de2e1a4ebbf6f3ddcb47adcf707be441
qiuyiliang.ok <qiuyiliang.ok@bytedance.com>
2022-03-10 14:10:21 +0800

    refactor rpc struct

--- raft/rpc.go
+++ raft/rpc.go
@@ -12,35 +12,6 @@ type RequestVoteReply struct {
 	VoteGranted bool
 }
 
-// RequestVote is the RPC sent by CANDIDATE.
-func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
-	rf.mu.Lock()
-	defer rf.mu.Unlock()
-
-	if args.Term < rf.currentTerm {
-		reply.Term = rf.currentTerm
-		reply.VoteGranted = false
-		return
-	}
-
-	if args.Term > rf.currentTerm {
-		rf.role = FOLLOWER
-		rf.currentTerm = args.Term
-		rf.votedFor = -1
-	}
-
-	reply.Term = rf.currentTerm
-	reply.VoteGranted = false
-
-	if (rf.votedFor == -1 || rf.votedFor == args.CandidateId) &&
-		rf.isUpToDate(args.LastLogTerm, args.LastLogIndex) {
-		rf.votedFor = args.CandidateId
-		reply.VoteGranted = true
-		rf.grantVoteCh <- struct{}{}
-	}
-	rf.persist()
-}
-
 type AppendEntriesArgs struct {
 	Term         int
 	LeaderId     int
@@ -51,61 +22,9 @@ type AppendEntriesArgs struct {
 }
 
 type AppendEntriesReply struct {
-	Term         int
-	Success      bool
-	NextTryIndex int
-}
-
-// AppendEntries is the RPC sent by LEADER.
-func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) {
-	rf.mu.Lock()
-	defer rf.mu.Unlock()
-
-	reply.Success = false
-
-	if args.Term < rf.currentTerm {
-		reply.Term = rf.currentTerm
-		reply.NextTryIndex = rf.lastIndex() + 1
-		return
-	}
-
-	defer rf.persist()
-	if args.Term > rf.currentTerm {
-		rf.role = FOLLOWER
-		rf.currentTerm = args.Term
-		rf.votedFor = -1
-	}
-
-	rf.heartbeatCh <- struct{}{}
-	reply.Term = rf.currentTerm
-
-	if args.PrevLogIndex > rf.lastIndex() {
-		reply.NextTryIndex = rf.lastIndex() + 1
-		return
-	}
-
-	baseIndex := rf.log[0].Index
-	if args.PrevLogIndex >= baseIndex && args.PrevLogTerm != rf.log[args.PrevLogIndex-baseIndex].Term {
-		term := rf.log[args.PrevLogIndex-baseIndex].Term
-		for i := args.PrevLogIndex - 1; i >= baseIndex; i-- {
-			if rf.log[i-baseIndex].Term != term {
-				reply.NextTryIndex = i + 1
-				break
-			}
-		}
-	} else if args.PrevLogIndex >= baseIndex-1 {
-		rf.log = rf.log[:args.PrevLogIndex-baseIndex+1]
-		rf.log = append(rf.log, args.Entries...)
-
-		reply.Success = true
-		reply.NextTryIndex = args.PrevLogIndex + len(args.Entries)
-
-		if rf.commitIndex < args.LeaderCommit {
-			// update commitIndex and apply log
-			rf.commitIndex = min(args.LeaderCommit, rf.lastIndex())
-			go rf.applyLog()
-		}
-	}
+	Term       int
+	Success    bool
+	RejectHint int
 }
 
 type InstallSnapshotArgs struct {
@@ -118,35 +37,4 @@ type InstallSnapshotArgs struct {
 
 type InstallSnapshotReply struct {
 	Term int
-}
-
-// InstallSnapshot is the RPC of creating a snapshot.
-func (rf *Raft) InstallSnapshot(args *InstallSnapshotArgs, reply *InstallSnapshotReply) {
-	rf.mu.Lock()
-	defer rf.mu.Unlock()
-
-	if args.Term < rf.currentTerm {
-		reply.Term = rf.currentTerm
-		return
-	}
-
-	if args.Term > rf.currentTerm {
-		rf.role = FOLLOWER
-		rf.currentTerm = args.Term
-		rf.votedFor = -1
-		rf.persist()
-	}
-
-	rf.heartbeatCh <- struct{}{}
-	reply.Term = rf.currentTerm
-
-	if args.LastIncludedIndex > rf.commitIndex {
-		rf.trimLog(args.LastIncludedIndex, args.LastIncludedTerm)
-		rf.lastApplied = args.LastIncludedIndex
-		rf.commitIndex = args.LastIncludedIndex
-		rf.persister.SaveStateAndSnapshot(rf.getPersist(), args.Data)
-
-		msg := ApplyMsg{UseSnapshot: true, Snapshot: args.Data}
-		rf.applyCh <- msg
-	}
 }
